233c92b546f861daa18822160dcff44bd89f549c
Jiwonp12 <ndwoo33@gmail.com>
2023-09-20 03:17:13 +0900

    feat: my page

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -3,22 +3,12 @@ import { connectDB } from "../../util/database";
 
 const page = async () => {
   const db = (await connectDB).db("forum");
-  const dataArray = await db
-    .collection("post")
+  const data: Post[] = await db
+    .collection<Post>("post")
     .find()
     .sort({ date: -1 })
     .toArray();
 
-  const data: Post[] = dataArray.map(item => ({
-    _id: item._id,
-    title: item.title,
-    content: item.content,
-    author: item.author,
-    category: item.category,
-    comment: item.comment,
-    date: item.date,
-    like: item.like,
-  }));
   return (
     <article className="w-full h-full mt-6 flex flex-col items-center justify-start">
       <Item data={data} />
